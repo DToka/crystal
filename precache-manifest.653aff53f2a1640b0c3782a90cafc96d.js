131a6c569d1ea85abaa7e595ccbc9ea284449f20
Dmitry Tokarev <csdtokarev@gmail.com>
2021-01-10 19:39:31 -0500

    Updates

---
+++ precache-manifest.653aff53f2a1640b0c3782a90cafc96d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73624c5b60d117236e13e3d1b638dc41",
+    "url": "/crystal/index.html"
+  },
+  {
+    "revision": "9f9834a5139bc50588e2",
+    "url": "/crystal/static/css/main.dea5c0e5.chunk.css"
+  },
+  {
+    "revision": "da58da7bb19337586e4d",
+    "url": "/crystal/static/js/2.e4adb0d4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crystal/static/js/2.e4adb0d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9834a5139bc50588e2",
+    "url": "/crystal/static/js/main.33307c0a.chunk.js"
+  },
+  {
+    "revision": "37cd6f297dba95374e02",
+    "url": "/crystal/static/js/runtime-main.b0fc884d.js"
+  }
+]);
